b5a9069a60ceb2e2e7c97cc22947e4294d2c8db8
Tekin Birdüzen <t.birduezen@web-coding.eu>
2020-10-02 17:37:10 +0200

    Filesystem Module started Worked on the internal Choices module

--- MNG/internal/choices.js
+++ MNG/internal/choices.js
@@ -4,9 +4,14 @@ module.exports = {
     getChoicesImages(_season, _chapter, _part) {
 
     },
-    search(searchstring) {
-        // First of all get the choices dir
-        let _choicesDir = BOT.internal.get("fs").getChoicesDir();
+    search(_season, _chapter, _part) {
+        // Fill the 1 digit numbers with a 0
+        let [_zSeason, _zChapter, _zPart] = [_season, _chapter, _part].map(function(_number) {
+            return BOT.internal.get("data").zerofill(_number);
+        });
+        // Find the file
+        let _file = BOT.internal.get("fs").getChoiceFile(_zSeason, _zChapter, _zPart);
+
     },
     execute(msg, args) {
         msg.reply('pong');
--- MNG/internal/fs.js
+++ MNG/internal/fs.js
@@ -5,6 +5,21 @@ module.exports = {
     getChoicesDir() {
         return this.choicesDir;
     },
+    getChoiceFile(_season, _chapter, _part) {
+      let _file = FS.readdirSync(this.getChoicesDir()).filter(function(_filename) {
+          return 0 === _filename.indexOf(_searchstring);
+      })
+    },
+    /**
+     * Method to passte together the filename
+     * @param _season
+     * @param _chapter
+     * @param _part
+     * @returns {string}
+     */
+    createChoiceFilename(_season, _chapter, _part) {
+        return _season + "-" + _chapter + "+" + _part + ".png";
+    },
     execute(msg, args) {
         msg.reply('pong');
         msg.channel.send('pong');
