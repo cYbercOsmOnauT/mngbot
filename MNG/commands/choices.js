edcf17a2dd4059abf433cdaad5d3d16dccf090b6
Tekin Birdüzen <t.birduezen@web-coding.eu>
2020-11-21 21:50:24 +0100

    Recognize when a picture is missing.

--- MNG/commands/choices.js
+++ MNG/commands/choices.js
@@ -1,7 +1,11 @@
 module.exports = {
     name: 'choices',
     description: 'Show the choices of a specific part!',
-    execute(_msg, _args) {
+    execute(_msg, _commandline) {
         let _image = BOT.internal.get("choices").getChoicesImage(_args[0], _args[1], _args[2]);
+        if (!_image) {
+            // No choices image found
+            throw new Error("No such choice");
+        }
     },
 };
--- MNG/internal/choices.js
+++ MNG/internal/choices.js
@@ -2,7 +2,11 @@ module.exports = {
     name: 'choices',
     description: 'Calls to find choices',
     getChoicesImages(_season, _chapter, _part) {
-
+        let _image = BOT.internal.get("fs").getChoicesImage(_season, _chapter, _part);
+        if (!_image) {
+            // nothing found
+            return false;
+        }
     },
     search(_season, _chapter, _part) {
         // Fill the 1 digit numbers with a 0
